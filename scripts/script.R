f1dd6c6dac5f8022dc457d6d64628a8444d89b06
Cami <157552093+Carialva@users.noreply.github.com>
2024-05-06 20:01:36 -0500

    Add files via upload

---
+++ scripts/script.R
@@ -0,0 +1,56 @@
+# 
+# Big Data y Machine Learning para Economia Aplicada
+# Maria Camila Arias, Martin Velasquez, Mario Velasquez, Daniela Vlasak
+# Problem Set 2
+# 
+
+# Initial Setup -----------------------------------------------------------
+
+rm(list = ls())
+
+if(!require(pacman)) install.packages("pacman") ; require(pacman)
+p_load(rio, # import/export data
+       tidyverse, # tidy-data
+       skimr, # summary data
+       gridExtra, ## visualizing missing data
+       corrplot, ## Correlation Plots 
+       stargazer, ## tables/output to TEX. 
+       MASS,
+       rvest,
+       httr,
+       dplyr,
+       ggplot2,
+       visdat,
+       caret)
+
+
+# 1: Initial Data Manipulation -----------------------------------------------
+setwd("C:/Users/Maria.Arias/OneDrive - Universidad de los andes/MSc Economics/Big Data & Machine Learning/Problem set 3/data")
+train <- read.csv("train.csv")
+test <- read.csv("test.csv")
+str(test)
+
+train_miss <- skim(train)
+test_miss <- skim(test)
+print(train_miss)
+print(test_miss)
+# Surface, rooms and bathrooms have significant missings.
+
+###############################################################################
+#                                    Imputation                               
+###############################################################################
+train %>% count(bathrooms)
+ggplot(data=train,aes(x=bedrooms, y=bathrooms)) + geom_point()
+ggplot(data=train,aes(x=rooms, y=bathrooms)) + geom_point()
+filtered_data <- subset(train, surface_total < 200)
+ggplot(data=filtered_data,aes(x=surface_total, y=bathrooms)) + geom_point()
+
+##There is no lineal sense with the bathrooms variable, so, I will 
+
+
+#Explore data
+prop.table(table(train$property_type)) ## In the training set 76% are apartments
+prop.table(table(test$property_type)) ## While in the test set 97% are apartments
+## Hence in Chapinero people rarely sell houses.
+
+
